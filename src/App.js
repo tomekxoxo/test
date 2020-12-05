7e0b8762b9f4cd0e4d2d522cc8ddc2bf7cef0351
tomasz.kasprowicz <tomasz.kasprowicz@rsqtechnologies.com>
2020-12-05 22:11:57 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import { Map, List, Set } from "immutable";
+import moment from "moment";
 
 function App() {
+  // const map1 = Map({ a: 1, b: 2, c: 3 });
+  // const map2 = map1.set('b', 50);
+  // console.log(map1.get('b'), map2.get('b')); // 2 vs. 50
+  // const map3 = map2.set('a', 6777);
+  // console.log(map3.get('a'));
+  // const map4 = Map({ "d": 1, "e": 2 });
+  // map4.merge(map3);
+  // console.log(map4.get('d'));
+  // console.log(map4);
+  // const set1 = Set([1, 2, 3, 3]);
+  // console.log(set1);
+  // const currentDate = moment(`12 2020`, 'MM YYYY');
+  // console.log(moment(currentDate).startOf('month'));
+  let numOfDays = new Date(2020, 10, 0).getDate();
+  const currYear = 2020;
+  const currMonth = new Date().getMonth();
+  const firstDay = new Date(currYear, currMonth, 1).getDay() ;//indexed from sunday
+  console.log(numOfDays);
+  console.log(firstDay);
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>ASD<p>ttt</p></h1>
+  )
 }
+
 
 export default App;
